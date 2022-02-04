0381f8df976f67e802dadf9deaf63ba0b16b6e8c
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2022-02-04 23:27:51 +0100

    Check for the query length!

--- notflix.js
+++ notflix.js
@@ -3,6 +3,11 @@ const { spawn } = require('child_process');
 const clivas = require('clivas');
 
 let query = process.argv.slice(2).join(" ");
+if (!query || query.length <= 2) {
+    console.log(`\x1b[31mNo query given\x1b[0m`);
+    process.kill(process.pid);
+    return
+}
 (async () => {
     const movie = await fetch(`https://1337x.wtf/search/${query}/1/`).then(async (res) => await res.text()).then(res => res.match(/torrent\/[0-9]{7}\/[a-zA-Z0-9?%-]*/g)[0])
     const magnet = await fetch(`https://1337x.wtf/${movie}/`).then(async (res) => await res.text()).then((res) => res.match(/magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+/gi)[0].split("\"")[0])
